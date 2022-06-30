80a0e9d706f20f1b752007cde6800557b12a2797
andrektozzi <andrektozzi@gmail.com>
2022-06-30 13:17:47 -0300

    adicionada rota post saidas

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import joi from "joi";
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import dayjs from "dayjs";
 
 dotenv.config();
 
@@ -120,6 +122,45 @@ app.post("/entradas", async (req, res) => {
     }
 });
 
+app.post("/saidas", async (req, res) => {
+    const { authorization } = req.headers;
+    const { value, description, type } = req.body;
+    const data = dayjs().format("DD/MM");
+    const token = authorization?.replace("Bearer ", "");
+
+    const saidasSchema = joi.object({
+        value: joi.number().required(),
+        description: joi.string().required(),
+        type: joi.string().valid("saidas").required
+    });
+
+    const validacao = saidasSchema.validate({ value, description, type }, { aboutEarly: false });
+
+    if(validacao.error) {
+        return res.sendStatus(422);
+    }
+
+    try {
+        const sessao = await db.collection("sessoes").findOne({ token });
+
+        if(!sessao) {
+            return res.sendStatus(401);
+        }
+
+        const transacoes = await db.collection("transacoes").insertOne({
+            value,
+            description,
+            type,
+            date,
+            userId: sessao.userId
+        });
+
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+})
+
 app.get("/transacoes", async (req,res) => {
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
@@ -137,6 +178,8 @@ app.get("/transacoes", async (req,res) => {
         res.sendStatus(500);
     }
 });
+
+
 
 app.listen(5000, () => console.log("Servidor rodando!"));
 
