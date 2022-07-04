824177e6715a53bc5e9c9798265691a4cf927536
andrektozzi <andrektozzi@gmail.com>
2022-07-04 13:31:59 -0300

    adicionada lógica password

--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -2,8 +2,9 @@ import joi from "joi";
 
 export const authSignUpSchema = joi.object({
     name: joi.string().required(),
-    email: joi.string().required(),
-    password: joi.string().required()
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+    confirmPassword: joi.any().valid(joi.ref("password")).required()
 });
 
 export const authLoginSchema = joi.object({
